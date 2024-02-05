1b77e0047d59925d6dbbce7b16313a53f0c612c2
amaresh2507 <amareshs013@gmail.com>
2024-02-05 05:33:52 +0000

    placeholder tests added

--- tests/test_login.py
+++ tests/test_login.py
@@ -6,29 +6,39 @@ from assertpy import assert_that
 from selenium.webdriver.common.by import By
 
 
-class TestLogin:
-
-    @pytest.fixture(scope="function",autouse=True)
+class WebDriverWrapper():
+    @pytest.fixture(scope="function", autouse=True)
     def setup(self):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         self.driver.implicitly_wait(5)
         self.driver.get("http://demo.openemr.io/b/openemr/")
+
         yield
         time.sleep(5)
         self.driver.quit()
 
-    def test_Title(self):
 
+class TestLogin(WebDriverWrapper):
+
+    def test_Title(self):
         title_page = self.driver.title
         # assert title_page == "OpenEMR Login"
         assert_that(title_page).is_equal_to("OpenEMR Logins")
         print(title_page)
 
     def test_Desc(self):
-
         text = self.driver.find_element(By.XPATH, "//p[@class='text-center lead']").text
         assert_that(text).contains("Electronic Health Record and Medical Practice Management")
-        self.driver.quit()
 
-#master branch pushed
+    def test_Placeholder_Usr(self):
+        placeholder_usr = self.driver.find_element(By.XPATH, "//input[@placeholder='Username']").get_attribute('placeholder')
+        print(placeholder_usr)
+        assert_that(placeholder_usr).is_equal_to('Username')
+
+    def test_Placeholder_Pwd(self):
+        placeholder_pwd = self.driver.find_element(By.XPATH, "//input[@placeholder='Password']").get_attribute('placeholder')
+        print(placeholder_pwd)
+        assert_that(placeholder_pwd).is_equal_to('Passwords')
+
+# master branch pushed
